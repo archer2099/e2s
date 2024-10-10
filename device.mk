d653977c8f1ecfaeaef6c254f3d7154c1b48f312
ㅤㅤㅤ <nzero0973@gmail.com>
2024-10-10 16:20:52 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,11 @@
 #
 
 LOCAL_PATH := device/samsung/e2s
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
