92911fdd591c859ebab17ad1a7ec622aa17f78c8
ㅤㅤㅤ <nzero0973@gmail.com>
2024-10-10 16:23:43 +0700

    Update twrp_e2s.mk

--- twrp_e2s.mk
+++ twrp_e2s.mk
@@ -5,9 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := e2s
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -15,12 +22,17 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from e2s device
 $(call inherit-product, device/samsung/e2s/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/e2s/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := e2s
 PRODUCT_NAME := twrp_e2s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-S926B
 PRODUCT_MANUFACTURER := samsung
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
